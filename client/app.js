34752b619b87430c9ac42aa6f3719e8a11fe27c1
Huang Lie Jun <liejun.hlj@gmail.com>
2016-09-26 08:42:07 +0800

    Routing and Default Page Layout

--- client/app.js
+++ client/app.js
@@ -5,16 +5,17 @@ import ReactDOM from 'react-dom'
 // Routing
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
-import routes from './routes/route';
+import routes from './routes/route.jsx';
 
 // Import jQuery and Bootstrap
 global.$ = require('jquery');
-var bootstrap = ('bootstrap');
+global.jQuery = require('jquery');
+var bootstrap = require('bootstrap');
 
 // Import Views
-import App from './views/app-view';
-import ContactList from "./views/list-view" // For Contact List page
-import ContactForm from "./views/form-view" // For Add/Edit page
+import App from './views/app-view.jsx';
+import ContactList from "./views/list-view.jsx" // For Contact List page
+import ContactForm from "./views/form-view.jsx" // For Add/Edit page
 
 // Import Data Model
 var contact = require("./models/contact"); // Data structure to store contact information
@@ -23,5 +24,5 @@ var contact = require("./models/contact"); // Data structure to store contact in
 require("bootstrap/dist/css/bootstrap.css");
 require("./styles/main.css");
 
-// Routing in a single-page application
+// Routings
 render(<Router routes={routes} history={browserHistory} />, document.getElementById('main'));
